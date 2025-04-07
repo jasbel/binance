3646d11771b9aa36714806f8dfdbb9ffe57c23cf
asbel.dev <asbel.elmec@gmail.com>
2025-04-07 21:21:05 +0200

    add grafic

--- src/controllers/data.controller.ts
+++ src/controllers/data.controller.ts
@@ -2,11 +2,31 @@ import { Request, Response } from "express";
 import { capturePrice, Price } from "../cron";
 
 export const getCapture = async (_: Request, res: Response) => {
-    const currentInfo = await capturePrice()
-    res.json({ ok: 'generate', data: currentInfo });
-}
+  const currentInfo = await capturePrice();
+  res.json({ ok: "generate", data: currentInfo });
+};
 
 export const getPrices = async (_: Request, res: Response) => {
-    const prices = await Price.findAll({ order: [["createdAt", "DESC"]], limit: 10 });
-    res.json(prices);
-}
+  const prices = await Price.findAll({ order: [["createdAt", "DESC"]], limit: 10 });
+  res.json(prices);
+};
+export const getGrafic = async (_: Request, res: Response) => {
+  const formatFecha = (fechaStr: string) => {
+    const dias = ["D", "L", "M", "X", "J", "V", "S"]; // Domingo a Sábado
+    const fecha = new Date(fechaStr);
+    // rawData.map(item => new Date(item.createdAt).toLocaleString());
+
+    const diaSemana = dias[fecha.getDay()];
+    const diaMes = fecha.getDate();
+    const horas = fecha.getHours().toString().padStart(2, "0");
+    const minutos = fecha.getMinutes().toString().padStart(2, "0");
+
+    return `${diaSemana} (${diaMes}) ${horas}:${minutos}`;
+  };
+
+  const prices = await Price.findAll({ order: [["createdAt", "DESC"]], limit: 100 });
+
+  const pricesArr = prices.map((it) => it.dataValues).map(it=> ({...it, createdAt: formatFecha(it.createdAt)}));
+
+  res.render("grafico", { data: JSON.stringify(pricesArr) });
+};
--- src/routes/data.routes.ts
+++ src/routes/data.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getCapture, getPrices } from "../controllers/data.controller";
+import { getCapture, getGrafic, getPrices } from "../controllers/data.controller";
 
 const dataRoutes = Router()
 
 dataRoutes.get("/prices", getPrices);
 dataRoutes.get("/capture", getCapture);
+
+dataRoutes.get('/grafico', getGrafic);
 
 export default dataRoutes
